f4f04a6c7094852adeeec40087b2ba97fd54dec5
baconbotmk1 <54626636+baconbotmk1@users.noreply.github.com>
2021-06-08 08:13:33 +0200

    Add project files.

---
+++ Rent-A-Car/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rent_A_Car
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
